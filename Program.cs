d52277c797924677cad5e9199b05b48aaeff7faf
edknight91 <ed.knight1991@gmail.com>
2021-10-21 18:55:45 +0100

    intial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
